6a6695d2ae6e23b535bd2392f5419c177588d701
itvius <itvius@gmail.com>
2018-03-03 01:20:17 +0400

    Fix problem with mobile version and add scroll

--- Demo_project_1/js/common.js
+++ Demo_project_1/js/common.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
 
+		$(document).on("scroll", onScroll);
+ 
+		$('.top_mnu').on('click','a', function (e) {
+			e.preventDefault();
+			/*$(document).off("scroll");*/
+ 
+			/*$('a').each(function () {
+				$(this).removeClass('active');
+			})
+			$(this).addClass('active');*/
+ 
+			var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+			$('html, body').animate({scrollTop: top-60}, 
+				700, 'swing', function () {
+				$(document).on("scroll", onScroll);
+			});
+		});
+ 
+	function onScroll(event){
+		var scrollPosition = $(document).scrollTop()+90;
+		$('.top_mnu a').each(function () {
+			var currentLink = $(this);
+			var refElement = $(currentLink.attr("href"));
+			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('.top_mnu ul li a').removeClass("active");
+				currentLink.addClass("active");
+			}
+		});
+	}
+	
+    $("#header").on("click","button",  function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top - 60}, 900);
+    });
+
 	var containerEl = document.querySelector('#mixup_grid')
 
 	var mixer = mixitup(containerEl, {
@@ -16,8 +54,8 @@ $(document).ready(function() {
 		}
 	});
 
-	$(".section_mixup li").click(function() {
-		$(".section_mixup li").removeClass("active");
+	$(".section_mixup li" || ".top_mnu li a").click(function() {
+		$(".section_mixup li" || ".top_mnu li a").removeClass("active");
 		$(this).addClass("active");
 	});
 
@@ -31,8 +69,6 @@ $(document).ready(function() {
 		$(this).find("a").attr("href", "#work_" + i);
 		$(this).find(".podrt_descr").attr("id", "work_" + i);
 	});
-
-	$('head','watch').css('min-height', $(window).height());
 });
 
 document.body.onload = function() {
@@ -44,13 +80,3 @@ document.body.onload = function() {
 		}
 	}, 1000);
 };
-
-/*var containerEl = document.querySelector('#mixup_grid');
-
-var mixer = mixitup(containerEl, {
-	animation: {
-		enable:true,
-		effects:'scale fade',
-		duration:700
-	}
-});*/
