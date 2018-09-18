e748a588569f5347f07d1311b468af76daed94dd
itvius <itvius@gmail.com>
2018-09-18 22:36:57 +0400

    First step

---
+++ Compl_test/php/page.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_GET['subreddit'])){
+    $sub = $_GET['subreddit'];
+} else {
+    echo 'no sub found';
+}
+$data = file_get_content("http://reddit.com/.json");
+echo $data;
+
+?>
