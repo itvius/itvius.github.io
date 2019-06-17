ad7f6fae38be1867944e6b143904d68cb6d40e82
itvius <itvius@gmail.com>
2019-06-17 22:45:46 +0400

    Test page for Direck Line

---
+++ TestDev/js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+    $(document).on("scroll", onScroll);
+    var menu = document.querySelectorAll('.top_mnu, .bottom_mnu');
+
+    $(menu).on('click', 'a', function (e) {
+        e.preventDefault();
+
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('html, body').animate({scrollTop: top - 60},
+            700, 'swing', function () {
+                $(document).on("scroll", onScroll);
+            });
+    });
+
+    function onScroll(event){
+        var scrollPosition = $(document).scrollTop()+90;
+        $('.top_mnu a').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+                $('.top_mnu ul li a').removeClass("active");
+                currentLink.addClass("active");
+            }
+        });
+    };
+
+});
