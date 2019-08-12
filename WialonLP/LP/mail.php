71e00df11b187e32678532c695ae8f4e72bd1c8c
dg <dg@nekta.tech>
2019-08-12 10:07:58 +0400

    add second modal

--- WialonLP/LP/mail.php
+++ WialonLP/LP/mail.php
@@ -1,13 +1,15 @@
 <?php
 $phone = $_POST['phone'];
 $name = $_POST['name'];
+$email = $_POST['email'];
 $desctext='Заявка с сайта';
 if (isset($_POST['name'])) {$name = $_POST['name'];}
-if (isset($_POST['tel'])) {$phone = $_POST['phone'];}
+if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+if (isset($_POST['email'])) {$email = $_POST['email'];}
 $to = "sales@wialon-service.ru";
 $headers = "Content-type: text/plain; charset = utf-8";
 $subject = "Заявка с Вашего сайта 'Лэндос для выставки'";
-$message = $desctext . "\nИмя пославшего:"  . $name . "\nТелефон:"  . $phone;
+$message = $desctext . "\nИмя пославшего:"  . $name . "\nТелефон:"  . $phone . "\nEmail:" . $email;
 if(mail ($to, $subject, $message, $headers)){
 ?>
 <script type="text/javascript">
